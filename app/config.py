dba8fbf686ab401b8bf7486d9e53721a0687fe21
thomaslaurenson <thomas@thomaslaurenson.com>
2024-12-20 08:05:04 +1300

    Added base code for Tenable vuln query and Varroa security risk creation

---
+++ app/config.py
@@ -0,0 +1,37 @@
+# Define the CIDR ranges for the Auckland AZ in Nectar Cloud
+TARGET_CIDR = None
+
+
+# Change me to desired security risk type
+SECURITY_RISK_TYPE_TARGET = "password-ssh"
+
+
+# Set lookup dictionary for security risk types
+SECURITY_RISK_TYPE_LOOKUP = {
+    "vulnerable-http": "b90e99dd-b49d-4110-8d1b-d1581ca474b4",
+    "password-ssh": "c59f20bd-cd38-4fd4-be8f-ae2ea88b0460",
+    "accessible-db": "cb9feb63-88ba-4016-91ed-347e8806aed8",
+    "accessible-rdp": "db27aeb2-6e0f-4aa3-8440-043f131bf979",
+}
+
+
+# Set lookup dictionary for Tenable plugin IDs
+SECURITY_RISK_TYPE_TENABLE_PLUGINS = {
+    "vulnerable-http": None,
+    "password-ssh": [
+        149334,  # SSH Password Authentication Accepted
+    ],
+    "accessible-db": [
+        2131,  # SQL Server Detection
+        9508,  # Microsoft SQL Server 2008 Detection
+        9510,  # Microsoft SQL Server 2012 Detection
+        9512,  # Microsoft SQL Server 2016 Detection
+        7220,  # Microsoft SQL Server Database Instance Detection
+        122583,  # SQL Server Version Detection
+    ],
+    "accessible-rdp": [
+        5954,  # Windows RDP / Terminal Services Detection
+        10940,  # Remote Desktop Protocol Service Detection
+        27507,  # OS Identification : RDP
+    ],
+}
---
+++ app/create_security_risks.py
@@ -0,0 +1,59 @@
+import tenable_helpers
+import varroa_helpers
+
+
+def main():
+    # Check we have the required Nectar Cloud credentials
+    has_env_vars = varroa_helpers.openstack_check_env_vars()
+    if not has_env_vars:
+        print("[!] Missing OpenStack credentials... Exiting.")
+        exit()
+
+    # Check we have the required Tenable Cloud credentials
+    has_env_vars = tenable_helpers.tenable_check_env_vars()
+    if not has_env_vars:
+        print("[!] Missing Tenable credentials... Exiting.")
+        exit()
+
+    # Get all vulnerability findings for:
+    # - When the finding is in the specified CIDR range
+    # - When the finding is password-based SSH access
+    print("[*] Fetching vulnerabilities from Tenable...")
+    findings = tenable_helpers.get_findings_based_on_cidr_and_vulns()
+
+    for finding in findings:
+        print(f"[*] Processing: {finding['ip_address']}...")
+
+        # Skip any finding where the state is not OPEN
+        if finding["state"] != "OPEN":
+            print("    [!] Finding is not OPEN... Skipping.")
+            continue
+
+        # Get the instance ID and project ID based on the IP address and date
+        lookup_result = varroa_helpers.get_instance_based_on_ip_history(finding["ip_address"], finding["last_found"])
+        if lookup_result[0] is None:
+            print("    [!] No instance found... Skipping.")
+            continue
+        else:
+            print("    [*] Found instance...")
+            finding["instance_id"] = lookup_result[0]
+            finding["project_id"] = lookup_result[1]
+
+        # Check if the security risk already exists
+        security_risk_exists = varroa_helpers.check_security_risk_exists(finding)
+        if security_risk_exists:
+            print("    [!] Security risk already exists... Skipping.")
+            continue
+        else:
+            print("    [*] Security risk does not exist...")
+
+        # Create the security risk
+        result = varroa_helpers.create_security_risk(finding)
+        if result:
+            print("    [*] Security risk created.")
+        else:
+            print("    [!] Failed to create security risk.")
+
+
+if __name__ == "__main__":
+    main()
